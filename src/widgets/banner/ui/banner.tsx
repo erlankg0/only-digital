099c296169d56ed4ddeae6d918b5d9e7c74fe74c
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2025-08-07 15:55:38 +0300

    base cicle slide

--- src/widgets/banner/ui/banner.tsx
+++ src/widgets/banner/ui/banner.tsx
@@ -1,13 +1,11 @@
 import styles from './banner.module.scss';
 import { TitleUI } from '@/shared/components/title';
-import { SliderUI } from '@/widgets/slider';
 import { CircleSlider } from '@/widgets/circle-slider';
 
 export function Banner() {
   return (
     <section className={styles.banner}>
       <TitleUI title={'Исторические даты'} />
-      <SliderUI />
       <CircleSlider />
     </section>
   );
--- src/widgets/circle-slider/ui/circle-slider.tsx
+++ src/widgets/circle-slider/ui/circle-slider.tsx
@@ -1,7 +1,63 @@
+'use client';
+
+import { useRef, useState } from 'react';
+import gsap from 'gsap';
 import styles from './circle-slider.module.scss';
 
+const ITEMS = Array.from({ length: 8 });
+
 export function CircleSlider() {
+  const circleRef = useRef<HTMLDivElement>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const anglePerItem = 360 / ITEMS.length;
+
+  const rotateTo = (index: number) => {
+    const currentRotation = gsap.getProperty(circleRef.current, "rotation") as number;
+    const targetAngle = -anglePerItem * index + 90;
+
+    let angleDiff = targetAngle - currentRotation;
+    while (angleDiff > 180) angleDiff -= 360;
+    while (angleDiff < -180) angleDiff += 360;
+
+    const newRotation = currentRotation + angleDiff;
+
+    gsap.to(circleRef.current, {
+      rotation: newRotation,
+      duration: 0.6,
+      ease: 'power2.inOut',
+    });
+
+    setActiveIndex(index);
+  };
+
+
   return (
-    <div className={styles.slider}></div>
+    <div className={styles.wrapper}>
+      <div className={styles.circle} ref={circleRef}>
+        {ITEMS.map((_, i) => {
+          const angle = (360 / ITEMS.length) * i;
+          const radius = 265;
+
+          const x = radius * Math.cos((angle * Math.PI) / 180);
+          const y = radius * Math.sin((angle * Math.PI) / 180);
+
+          return (
+            <div
+              key={i}
+              className={`${styles.item} ${i === activeIndex ? styles.active : ''}`}
+              style={{
+                left: '50%',
+                top: '50%',
+                transform: `translate(${x}px, ${y}px) translate(-50%, -50%)`,
+              }}
+
+              onClick={() => rotateTo(i)}
+            >
+              {i + 1}
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
